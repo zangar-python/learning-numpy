7d28f8e7d1da04cd28a3668c1e1325558a1990f4
Zangar python dev. <zangar12334@gmail.com>
2025-10-08 18:25:44 +0500

    Create winners.py

---
+++ winners.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+user_with_scores = np.array([
+    [1,90],
+    [2,30],
+    [3,67],
+    [4,57],
+    [5,35]
+])
+#print("scores:",user_with_scores[:,1])
+
+winners = user_with_scores[:,1] > 50
+
+
+print(user_with_scores[winners]
